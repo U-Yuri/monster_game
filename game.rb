e11f7ec394bb2c173fdaaac0ebadfa951b40f1c8
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-07-05 17:57:39 +0900

    討伐に関するコード記載

--- game.rb
+++ game.rb
@@ -1,18 +1,43 @@
-monster = "うさぎーぬ"
+monster = "ピ チュウ"
+puts "#{monster}が出た！"
+
+monster_total_hp = 100
+pokemon_total_hp = 100
+money = 500
 
 while true
-puts "#{monster}が出た！"
-puts "■何をする？"
-puts "1: たたかう！"
-puts "2: アイテムを使う"
+    puts "■何をする？"
+    puts "1: たたかう！"
+    puts "2: アイテムを使う"
 
-puts = "1 or 2 ?"
-select = gets
+    puts = "1 or 2 ?"
+    select = gets
 
-damage = (1..90).to_a
-if select == "1\n"
-    dg = damage.sample
-    puts "リンリンに#{dg}のダメージを与えた"
-end
-
+    monster_damage = (1..90).to_a
+    pokemon_damage = (1..90).to_a
+    case select
+    when "1\n"
+        monster_damaged = monster_damage.sample
+        pokemon_damaged = pokemon_damage.sample
+        pokemon_total_hp -= monster_damaged
+        monster_total_hp -= pokemon_damaged
+        if pokemon_total_hp >= 0 && monster_total_hp >= 0
+            puts "ピ チュウはリンリンに#{monster_damaged}のダメージを与えた"
+            puts "リンリンのHPは#{pokemon_total_hp}になった"
+            puts "リンリンはピカチュウに#{pokemon_damaged}のダメージを与えた"
+            puts "ピ チュウのHPは#{ monster_total_hp}になった"
+        elsif pokemon_total_hp < 0
+            puts "ピ チュウはリンリンに#{monster_damaged}のダメージを与えた"
+            puts "リンリンは死んだ！"
+            puts "残念！あなたの負け！"
+            break
+        elsif  monster_total_hp < 0
+            puts "ピ チュウはリンリンに#{monster_damaged}のダメージを与えた"
+            puts "リンリンのHPは#{pokemon_total_hp}になった"
+            puts "リンリンはピカチュウに#{pokemon_damaged}のダメージを与えた"
+            puts "ピ チュウは死んだ！"
+            puts "あなたの勝利！"
+            break
+        end       
+    end
 end
